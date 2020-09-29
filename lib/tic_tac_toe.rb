f93924404a02d277ff4935caad2535a22789887e
Alan Young <alanryoung01@gmail.com>
2020-09-29 16:39:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,6 +53,25 @@ def turn
   end
 end
 
+def turn_count
+  turn = 0
+  @board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
+    end
+  end
+  return turn
+end
+
+def current_player
+  turns = turn_count
+  if turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
 
 end
 end
