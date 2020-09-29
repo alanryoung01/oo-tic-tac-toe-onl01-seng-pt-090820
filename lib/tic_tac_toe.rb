56a97051000b14af05910bb53f0294e9d0fae1aa
Alan Young <alanryoung01@gmail.com>
2020-09-29 16:34:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,4 +13,31 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [6, 4, 2],
 ]
+
+def display_board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(index, player = "X")
+  @board[index] = player
+end
+
+def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
+end
+
+def valid_move?(index)
+  if index.between?(0,8)
+  elsif !position_taken?(index)
+  end
+  
+
 end
